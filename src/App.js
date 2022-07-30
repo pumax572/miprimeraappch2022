9989cdaf51f4aa11d655ae27a742f636557cb75f
Daniel Vega <vegadm57@gmail.com>
2022-07-30 19:53:56 -0300

    Intruduciendome en el contador de futuros productos seleccionados

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';//Integremos el módulo de CSS a WebPack
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar'; //Los componentes los nombramos utilizando PascalCase
+import Counter from './components/Counter/Counter';
 
 function App() {
-  
+  const handleOnAdd = (quantity) => {
+    console.log(`la cantidad agregada es: ${quantity}`)
+  }
     return (
     <div className="App">
       <Navbar/> {/* Llamamos al Navbar */}
       <ItemListContainer greeting ='No soy taaaaaan responsive por ahora...'/>
+      <Counter stock={14} onAdd={handleOnAdd}/>
     </div>
     
   );
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,16 +1,32 @@
 //Creamos un contador para agregar o quitar productos del ecommerce
-//import { useState } from "react"; 
+import { useState } from "react"; 
 
 //useState = Este hook, nos permite guardar un pedacito de memoria para guardar un dato y que al ejecutarlo, react pueda ejecutar nuevamente todo el calculo y actualizar el DOM.
 
 //useEffect = Este hook sirve para controlar efectos secundarios y las etapas del ciclo de vida.
+//Este contador lo vamos a usar mas adelante con los productos del ecommerce
+const Counter = ({ stock, onAdd }) => {
+    const [count, setCount] = useState(1)
 
-//const Counter = ()=> {
+    const increment = () => {
+        if(count < stock ) {
+            setCount(count + 1)
+        }
+    }
 
-    
+    const decrement = () => {
+        if(count > 1) {
+            setCount(count - 1)
+        }
+    }
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={decrement}>Decrementar</button>
+            <button onClick={increment}>Incrementar</button>
+            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
 
-
-
-
-
-//}
+export default Counter
